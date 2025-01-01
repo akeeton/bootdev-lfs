b008cb409490d398f36f490ee869766f50abd8d7
akeeton <andrewrkeeton@gmail.com>
2025-01-01 11:16:31 -0500

    Only accept JPEG and PNG thumbnails

--- assets.go
+++ assets.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"mime"
 	"os"
 	"path/filepath"
 	"strings"
@@ -16,9 +17,13 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
-	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+func getAssetPath(videoID uuid.UUID, mediaType string) (string, error) {
+	ext, err := mediaTypeToExt(mediaType)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s%s", videoID, ext), nil
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
@@ -29,10 +34,25 @@ func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
 }
 
-func mediaTypeToExt(mediaType string) string {
+func mediaTypeToExt(contentyTypeHeader string) (string, error) {
+	mediaType, _, err := mime.ParseMediaType(contentyTypeHeader)
+	if err != nil {
+		return "nil", fmt.Errorf("error parsing media type from Content-Type header '%s': %w", contentyTypeHeader, err)
+	}
+
+	validMediaTypes := map[string]struct{}{
+		"image/jpeg": {},
+		"image/png":  {},
+	}
+
+	_, ok := validMediaTypes[mediaType]
+	if !ok {
+		return "", fmt.Errorf("invalid media type '%s'", mediaType)
+	}
+
 	parts := strings.Split(mediaType, "/")
 	if len(parts) != 2 {
-		return ".bin"
+		return "", fmt.Errorf("failed to get extension from media type '%s'", mediaType)
 	}
-	return "." + parts[1]
+	return "." + parts[1], nil
 }

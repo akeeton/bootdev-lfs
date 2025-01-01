fdb61b2dd43ec0df3c17e0faa05df6a9de438c92
akeeton <andrewrkeeton@gmail.com>
2025-01-01 11:43:22 -0500

    Add cache-busting query to thumbnail URL

--- app/app.js
+++ app/app.js
@@ -260,8 +260,8 @@ function viewVideo(video) {
   if (!video.thumbnail_url) {
     thumbnailImg.style.display = 'none';
   } else {
-    thumbnailImg.style.display = 'block';
-    thumbnailImg.src = video.thumbnail_url;
+    thumbnailImg.style.display = "block";
+    thumbnailImg.src = `${video.thumbnail_url}?v=${Date.now()}`;
   }
 
   const videoPlayer = document.getElementById('video-player');
